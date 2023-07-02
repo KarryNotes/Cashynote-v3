85269be71b24a754e53dc782c3d22b6d9c5d96e5
KarryNotes <kgupta.pvt@gmail.com>
2023-07-02 13:26:02 +0100

    dwa

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
 import sqlalchemy
-from sqlalchemy import create_engine, text
+from sqlalchemy import text
+from sqlalchemy import create_engine
 import ssl
 import os
 from os import environ
 
-db_connection_string = os.environ["DB_CONNECTION_STRING"]
+db_connection_string =  "mysql+pymysql://w760pvrule6vd6p9gqpz:pscale_pw_rUoEg4yD8GSNoLV9SjjdXp9BOOIkKrmuEflpIqKtWgz@aws.connect.psdb.cloud/cashynote?charset=utf8mb4" 
 
 engine = create_engine(
     db_connection_string,
@@ -14,7 +15,8 @@ def load_jobs_from_db():
    with engine.connect() as conn:
       result = conn.execute(text("select * from jobs")).all()
       jobs = []
+      
       for row in result:
         jobs.append(({'title':row.title, 'location':row.location, 'salary':row.salary}))
         print({'title':row.title, 'location':row.location, 'salary':row.salary})
-      return jobs
+        return jobs
