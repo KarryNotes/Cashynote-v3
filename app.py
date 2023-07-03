b20c53760f97c79568296ab6be8ff4eb85e82a52
KarryNotes <kgupta.pvt@gmail.com>
2023-07-03 17:50:16 +0100

    working DB

--- app.py
+++ app.py
@@ -1,23 +1,54 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from database import load_jobs_from_db
-
+from os import environ
+from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+from sqlalchemy import text
+import sqlalchemy
+import os
+load_dotenv()
 
 app = Flask(__name__)
 
+app.secret_key="cashynotes"
+# app.permanent_session_lifetime = timedelta(minutes=30)
+
+# DATABASE CONNECTION
+# Set & Get Env Variables
+dbUser = os.environ['DBUSERNAME'].strip()
+dbPwd = os.environ['PASSWORD'].strip()
+dbHost = os.environ['HOST'].strip()
+dbName = os.environ['DATABASE'].strip()
+
+dbConnectionStr = f"mysql+pymysql://{dbUser}:{dbPwd}@{dbHost}/{dbName}?ssl_ca=cacert.pem"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = dbConnectionStr
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+def load_jobs_from_db():
+  result = db.session.execute(text(f"select * from jobs")).all()
+  jobs = []
+
+  for row in result:
+      jobs.append(
+          ({"title": row.title, "location": row.location, "salary": row.salary})
+      )
+      print({"title": row.title, "location": row.location, "salary": row.salary})
+      return jobs
 
 @app.route("/")
 def hello():
     jobs = load_jobs_from_db()
     return render_template("home.html", jobs=jobs, company_name="Cashynote")
 
-
 @app.route("/api/jobs")
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -2,24 +2,4 @@ import sqlalchemy
 from sqlalchemy import text
 from sqlalchemy import create_engine
 import ssl
-import os
-from os import environ
 
-db_connection_string = "mysql+pymysql://etaz9j8b6c1irzuz5fvp:pscale_pw_WFQzD6cicM18jd08OorotjDSq4gTGIgotjt8KfkIkhx@aws.connect.psdb.cloud/cashynote"
-
-engine = create_engine(db_connection_string,
-    connect_args={"ssl": {"ssl_ca": "/etc/ssl/cert.pem"}},
-)
-
-
-def load_jobs_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from jobs")).all()
-        jobs = []
-
-        for row in result:
-            jobs.append(
-                ({"title": row.title, "location": row.location, "salary": row.salary})
-            )
-            print({"title": row.title, "location": row.location, "salary": row.salary})
-            return jobs
