a104dff8e675bd3465bab5c65ddaa9baedbab917
KarryNotes <kgupta.pvt@gmail.com>
2023-07-04 12:07:49 +0100

    dunno

--- database.py
+++ database.py
@@ -1,5 +1,17 @@
 import sqlalchemy
 from sqlalchemy import text
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, engine
 import ssl
 
+
+def load_job_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("select * from jobs where id = :val"),
+            val=id
+        )
+    rows= result.all()
+    if len(rows) == 0:
+        return None
+    else:
+        return dict(rows[0])
