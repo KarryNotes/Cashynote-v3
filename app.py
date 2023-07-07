3ae423c039c67fe7e7d8d5afd06c6b6ad2110919
KarryNotes <137776665+KarryNotes@users.noreply.github.com>
2023-07-07 11:48:41 +0000

    working website

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from os import environ
 from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
-import sqlalchemy
-from sqlalchemy.engine import Connection
-import connect
 import os
 from sqlalchemy import text
-from sqlalchemy import create_engine, engine
-import ssl
-
+from sqlalchemy import create_engine
 
 load_dotenv()
 
 app = Flask(__name__)
 
-app.secret_key="cashynotes"
+app.secret_key = "cashynotes"
 # app.permanent_session_lifetime = timedelta(minutes=30)
 
 # DATABASE CONNECTION
@@ -34,45 +28,54 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-def load_jobs_from_db():
-  result = db.session.execute(text(f"select * from cashynote.jobs")).all()
-  jobs = []
+db_connection_string = os.environ['DB_CONNECTION_STRING']
 
+engine = create_engine(dbConnectionStr)
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
   for row in result:
-      jobs.append(
-          ({"title": row.title, "location": row.location, "salary": row.salary, "currency": row.currency, "responsbilities": row.responsbilities, "requirements": row.requirements})
-      )
-      print({"title": row.title, "location": row.location, "salary": row.salary})
-      return jobs
-  
-def load_job_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(
-            text("select * from jobs where id = :val"),
-            val=id
-        )
-    rows= result.all()
+    jobs.append(({
+    "title": row.title,
+    "location": row.location,
+    "salary": row.salary,
+    "currency": row.currency,
+    "responsbilities": row.responsbilities,
+    "requirements": row.requirements
+  }))
+  print({"title": row.title, "location": row.location, "salary": row.salary,       "currency": row.currency, "responsbilities": row.responsbilities,"requirements": row.requirements})
+  return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
+    rows = result.all()
     if len(rows) == 0:
-        return None
+      return None
     else:
-        return dict(rows[0])
+      return dict(rows[0])
 
 @app.route("/")
 def hello():
-    jobs = load_jobs_from_db()
-    return render_template("home.html", jobs=jobs, company_name="Cashynote")
+  jobs = load_jobs_from_db()
+  return render_template("home.html", jobs=jobs, company_name="Cashynote")
+
 
 @app.route("/api/jobs")
 def list_jobs():
-    jobs = load_jobs_from_db()
-    return jsonify(jobs)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
 
 @app.route("/job/<id>")
 def show_job(id):
-    job = load_job_from_db()
-    return render_template('jobpage.html', job=job)
+  job = load_job_from_db(id)
+  return render_template('jobpage.html', job=job)
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
-
+  app.run(host="0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,17 +1,21 @@
-import sqlalchemy
 from sqlalchemy import text
-from sqlalchemy import create_engine, engine
-import ssl
+from sqlalchemy import engine
 
 
-def load_job_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(
-            text("select * from jobs where id = :val"),
-            val=id
-        )
-    rows= result.all()
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict[row])
+    return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), val=id)
+    rows = result.all()
     if len(rows) == 0:
-        return None
+      return None
     else:
-        return dict(rows[0])
+      return dict(rows[0])
