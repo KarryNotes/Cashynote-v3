b67a5ad069a0697f2c3b3a5c01646d25b2abf2d5
KarryNotes <137776665+KarryNotes@users.noreply.github.com>
2023-07-10 14:52:06 +0000

    working site

--- app.py
+++ app.py
@@ -9,10 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.secret_key = "cashynotes"
-# app.permanent_session_lifetime = timedelta(minutes=30)
 
-# DATABASE CONNECTION
-# Set & Get Env Variables
 dbUser = os.environ['DBUSERNAME'].strip()
 dbPwd = os.environ['PASSWORD'].strip()
 dbHost = os.environ['HOST'].strip()
@@ -29,12 +26,11 @@ db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(dbConnectionStr)
 
-
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
     jobs = []
-  for row in result:
+  for row in result.all():
     jobs.append(({
       "id": row.id,
       "title": row.title,
@@ -54,7 +50,6 @@ def load_job_from_db(id):
     else:
       return None
 
-
 def add_application_to_db(job_id, data):
   with engine.connect() as conn:
     query = text(
@@ -71,18 +66,15 @@ def add_application_to_db(job_id, data):
                resume_url=data['resume_url'])
   job_id = job_id
 
-
 @app.route("/")
 def hello():
   jobs = load_jobs_from_db()
   return render_template("home.html", jobs=jobs)
 
-
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
-
 
 @app.route("/job/<id>")
 def show_job(id):
@@ -91,15 +83,13 @@ def show_job(id):
     return "No job with given id exists!", 404
   return render_template('jobpage.html', job=job)
 
-
-@app.route("/job/<id>/apply", methods=['post'])
+@app.route('/job/<id>/apply', methods=['post'])
 def apply_to_job(id):
-  data = request.form
   job = load_job_from_db(id)
-  add_application_to_db(id, data)
-  return jsonify('application_submitted.html',
-                         applications=data,
-                         job=job)
+  data = request.form
+  return render_template('application_submitted.html', applications=data, job=job)
+  
+app.run(host='0.0.0.0', port=81, debug=True)
 
-if __name__ == "__main__":
-  app.run(host="0.0.0.0", debug=True)
+
+app.run(host="0.0.0.0", debug=True)
